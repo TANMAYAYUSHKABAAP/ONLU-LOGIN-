af9378ee649ad2729fdd331c0e3c9bad8e6d9628
TANMAYAYUSHKABAAP <morningstudy35@gmail.com>
2024-12-22 12:35:21 +0530

    Update and rename password.html to password.js

---
+++ password.js
@@ -0,0 +1,26 @@
+function getKeyForNumber(number) {
+    const keys = {
+        0: "TANMAYKING0",
+        1: "TANMAYKING1",
+        2: "TANMAYKING2",
+        3: "TANMAYKING3",
+        4: "TANMAYKING4",
+        5: "TANMAYKING5",
+        6: "TANMAYKING6",
+        7: "TANMAYKING7",
+        8: "TANMAYKING8",
+        9: "TANMAYKING9",
+        10: "TANMAYKING10",
+        11: "TANMAYKING11",
+        12: "TANMAYKING12",
+        13: "TANMAYKING13",
+        14: "TANMAYKING14",
+        15: "TANMAYKING15",
+        16: "TANMAYKING16",
+        17: "TANMAYKING17",
+        18: "TANMAYKING18",
+        19: "TANMAYKING19",
+        20: "TANMAYKING20"
+    };
+    return keys[number];
+}
